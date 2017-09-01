1e00a719e65449211a42c25cc428cddade1dee00
jeanpokou <pokoujeanmarc@gmail.com>
2017-09-01 12:04:40 -0300

    adding app code

---
+++ app/src/main/java/com/jeanpokou/retrofit/api/model/GitHubRepo.java
@@ -0,0 +1,13 @@
+package com.jeanpokou.retrofit.api.model;
+
+/**
+ * Created by User1 on 9/1/2017.
+ */
+
+public class GitHubRepo {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ app/src/main/java/com/jeanpokou/retrofit/api/service/GitHubClient.java
@@ -0,0 +1,13 @@
+package com.jeanpokou.retrofit.api.service;
+
+import com.jeanpokou.retrofit.api.model.GitHubRepo;
+import java.util.List;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+
+public interface GitHubClient {
+    @GET("/users/{user}/repos")
+    Call<List<GitHubRepo>> reposForUer(@Path("user") String user);
+}
